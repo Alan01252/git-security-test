ab3399a845d8f72ea09e9210d94a3bffaa01ec9b
Alan Hollis <me@alanhollis.com>
2013-10-03 22:15:36 +0100

    Add check for secure commit

--- isSecure.php
+++ isSecure.php
@@ -1,3 +1,10 @@
 <?php
-	$commits = exec('git rev-list --objects --all');
+	$out = [];
+	$commits = exec('git log --graph master | sed -n \'/^*/p\'', $out);
+	print_r($out);
+	if (in_array('* commit d13e40202835e6d132a22696b0d96648945a3e77', $out)) {
+		echo "is secure";
+	} else {
+		echo "isn't secure";
+	}
 ?>
